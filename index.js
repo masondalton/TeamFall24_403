148f135ee3f72b3d2883ca09f4769ddfcb2ba7ea
karensakuma19@gmail.com <karensakuma19@gmail.com>
2024-11-12 22:42:58 -0700

    added more code to the index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-let express = require("express");
-
-let app = express();
-
-let path = require("path");
-
+const express = require("express");
+const app = express();
+const path = require("path");
 const port = 2024;
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // Set EJS as the template engine
+app.set("views", path.join(__dirname, "views")); // Set the views folder path
 
-app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public')); // Ensure public files are accessible
 
-app.use(express.urlencoded({extended: true}));
+// Route for the root URL
+app.get("/", (req, res) => {
+    res.render("index"); // Render the 'index.ejs' file
+});
 
-app.listen(port, () => console.log('I am listening'));
+// Start the server
+app.listen(port, () => console.log('Listening!'));
